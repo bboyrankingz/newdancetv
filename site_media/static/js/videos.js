bebf161851bfde05e9f3ece951c44005c93df244
bboy.stats <bboy.stats@gmail.com>
2016-02-19 22:11:16 +0100

    manage and rm videos

--- site_media/static/js/videos.js
+++ site_media/static/js/videos.js
@@ -1,6 +1,6 @@
 var app = angular.module("newdancetv", ['ngRoute', 'infinite-scroll', 'ngCookies']);
 
-app.controller("Videos", function($scope, $http, $cookies, requester) {
+app.controller("Videos", function($scope, $http, $cookies, requester, Reddit) {
 
   var channels = ["ProDance TV", "TVlilou", "OckeFilms", "The Legits", "stance"];
   var token = "";
@@ -60,6 +60,22 @@ app.controller("Videos", function($scope, $http, $cookies, requester) {
 
   };
 
+  $scope.remove = function(id){
+      $http.defaults.headers.common['Authorization'] = $cookies.get("Token");
+      $http({
+          url: 'http://bboyrankingz.com/footage/' + id + '/',
+          method: 'DELETE'
+        }).
+        success(function(data) {
+          load_users_videos();
+        }).
+        error(function(data) {
+          //error but works ... use jsonp
+          load_users_videos();
+        });
+
+  };
+
   function load_users_videos()
   {
     $http.get('http://bboyrankingz.com/footage/list/.json?submitted_by__groups__name=newdancetv&referrer=newdance.tv').
@@ -67,10 +83,12 @@ app.controller("Videos", function($scope, $http, $cookies, requester) {
         $scope.tv["NewDanceTV Choices"] = data.results;
       });
 
-    $http.get('http://bboyrankingz.com/footage/list/.json?submitted_by__groups__name=none&referrer=newdance.tv').
-      success(function(data, status, headers, config) {
-        $scope.tv["User videos"] = data.results;
-      });
+    // $http.get('http://bboyrankingz.com/footage/list/.json?submitted_by__groups__name=none&referrer=newdance.tv').
+    //   success(function(data, status, headers, config) {
+    //     $scope.tv["User videos"] = data.results;
+    //   });
+
+    $scope.reddit = new Reddit('http://bboyrankingz.com/footage/list/.json?submitted_by__groups__name=newdancetv&referrer=newdance.tv');
   };
 
 
@@ -86,6 +104,35 @@ app.factory('requester', function($http) {
     };
     return { getData: getData };
 });
+
+// Reddit constructor function to encapsulate HTTP and pagination logic
+app.factory('Reddit', function($http) {
+  var Reddit = function($url) {
+    this.items = [];
+    this.busy = false;
+    this.after = $url;
+  };
+
+  Reddit.prototype.nextPage = function() {
+    if (this.busy) return;
+    if (this.after == null) return;
+    this.busy = true;
+
+    var url = this.after;
+      $http.get(url).success(function(data) {
+        var items = data.results;
+        for (var i = 0; i < items.length; i++) {
+          this.items.push(items[i]);
+        }
+        this.after = data.next;
+        this.busy = false;
+      }.bind(this));
+    
+  };
+
+  return Reddit;
+});
+
 
 
 app.config(function($routeProvider, $locationProvider) {
